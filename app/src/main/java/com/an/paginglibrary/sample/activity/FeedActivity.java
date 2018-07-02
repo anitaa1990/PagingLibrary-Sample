07daed5c0d9195261400a642437074961a8ebf15
anitaa <anitaa@airloyal.com>
2018-07-02 14:23:56 +0530

    #2 - Added comments to the activity class

--- app/src/main/java/com/an/paginglibrary/sample/activity/FeedActivity.java
+++ app/src/main/java/com/an/paginglibrary/sample/activity/FeedActivity.java
@@ -23,16 +23,40 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*
+         * Step 1: Using DataBinding, we setup the layout for the activity
+         *
+         * */
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        feedViewModel = new FeedViewModel(AppController.create(getApplicationContext()));
 
+        /*
+         * Step 2: Initialize the ViewModel
+         *
+         * */
+        feedViewModel = ViewModelProviders.of(this).get(FeedViewModel.class);
+
+        /*
+         * Step 2: Setup the adapter class for the RecyclerView
+         *
+         * */
         binding.listFeed.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         adapter = new FeedListAdapter(getApplicationContext());
 
+
+        /*
+         * Step 4: When a new page is available, we call submitList() method
+         * of the PagedListAdapter class
+         *
+         * */
         feedViewModel.getArticleLiveData().observe(this, pagedList -> {
             adapter.submitList(pagedList);
         });
 
+        /*
+         * Step 5: When a new page is available, we call submitList() method
+         * of the PagedListAdapter class
+         *
+         * */
         feedViewModel.getNetworkState().observe(this, networkState -> {
             adapter.setNetworkState(networkState);
         });
